8194971d6919a2a382f98585f71e55e9355e6f04
Greg Weber <gregoryweberguitars@gmail.com>
2018-01-18 15:09:16 -0800

    project using sessions for firtst time to count number of times that a user views the home page

---
+++ flask_fundamentals/counter/server.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request, redirect, session
+
+app = Flask(__name__)
+app.secret_key = 'ThisIsSecret'
+
+@app.route('/')
+def home():
+    return render_template('index.html', count=session['count'])
+
+@app.route('/count', methods=['POST'])
+def view_count():
+    session['count'] +=1
+    return redirect('/')
+app.run(debug=True)
+
+
