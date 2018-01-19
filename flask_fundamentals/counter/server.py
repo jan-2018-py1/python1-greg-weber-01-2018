f9da49feaa8902f541363febf8e9951594ed9393
Greg Weber <gregoryweberguitars@gmail.com>
2018-01-18 22:08:09 -0800

    added submit buttons to increase count by 2 and to reset count to 0

--- flask_fundamentals/counter/server.py
+++ flask_fundamentals/counter/server.py
@@ -1,16 +1,27 @@
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask, render_template, redirect, session
 
 app = Flask(__name__)
 app.secret_key = 'ThisIsSecret'
 
 @app.route('/')
 def home():
+    if 'count' not in session:
+        session['count'] = 0
+    session['count'] +=1
     return render_template('index.html')
 
-@app.route('/count', methods=['POST'])
-def view_count():
-    session['count'] +=1
+@app.route('/process', methods=['POST'])
+def submit_button():
+    session['count'] += 1
     return redirect('/')
+
+@app.route('/reset', methods=['POST'])
+def reset():
+    session['count'] = -1
+    return redirect('/')
+
 app.run(debug=True)
+
+
 
 
