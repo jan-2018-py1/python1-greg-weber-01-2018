9668db99e838c58e8328d637bb2b0c9f5fd235db
Greg Weber <gregoryweberguitars@gmail.com>
2018-01-20 14:52:16 -0800

    adding basic form validation to previous project

---
+++ flask_fundamentals/dojo_survey_with_validation/server.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request, redirect, session, flash
+app = Flask(__name__)
+app.secret_key = "vallllliiiid1at7ior"
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/results', methods=['POST'])
+def results():
+    if len(request.form['name']) < 1:
+        flash("***Name cannot be empty!***")
+        return redirect('/')
+    if len(request.form['location']) < 1:
+        flash("***Location cannot be empty!****")
+        return redirect('/')
+    if len(request.form['comment']) > 121:
+        flash("***comment cant be longer than 120!****")
+        return redirect('/')
+
+    return render_template('/results.html', name = request.form['name'], location = request.form['location'],  language = request.form['language'], comment = request.form['comment'], )
+
+@app.route('/return', methods=['POST'])
+def go_home():
+    return redirect('/')
+app.run(debug=True)
+
