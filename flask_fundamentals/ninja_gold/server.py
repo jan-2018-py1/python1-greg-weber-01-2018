8b0fe49c01c362b81b41e634aac27e8df204397c
Greg Weber <gregoryweberguitars@gmail.com>
2018-01-20 09:45:33 -0800

    commented out a session key that I wasn't using and lect comment note at top explaining thoughts on how I could improve my program

--- flask_fundamentals/ninja_gold/server.py
+++ flask_fundamentals/ninja_gold/server.py
@@ -3,6 +3,14 @@ app = Flask(__name__)
 app.secret_key = 'this_time_its_payback'
 import random 
 from time import ctime
+
+# note: I didn't use the session['mood'] key that I set up, I wanted to figuire out how to log all the activity in 
+# order regardless of getting gold or loseing gold, but since they need to have different text color I haven't 
+# fuigured how to do that yet. I made a different key in sessions for each activity - get gold or loose gold - and 
+# then logged that in my html one after the other.  My thought to log all regardless of color would be to store 
+# info in a list or dict and then append that to session['activity'] - so all info in one key of session rather
+#  than two ... so it would end up looking like [{'happy':'got gold'},{'sad':'lost gold'}, ....] -- then use some 
+# conditionals and loops to extract the correct info and display the correct text color.  
  
 @app.route('/')
 def home():
@@ -15,7 +23,7 @@ def home():
         session['casino_gold'] = 0
         session['got_gold'] = [] #a liist to store the activity log when we get gold
         session['lost_gold'] = [] #a liist to store the activity log when we loose gold
-        session['mood'] = '' #this will help us determine which activity to print
+        #session['mood'] = '' #this will help us determine which activity to print
     return render_template('index.html')
 
 @app.route('/process_money', methods=['POST'])
@@ -31,21 +39,21 @@ def gold_digger():
         session['gold_count'] += session['farm_gold']
         string = 'Earned {} gold from the farm! ({})'.format(session['farm_gold'], now)
         session['got_gold'].append(string)
-        session['mood'] = 'happy'
+        # session['mood'] = 'happy'
 
     if request.form['building'] == 'cave':
         session['cave_gold'] = random.randrange(5, 11)
         session['gold_count'] += session['cave_gold']
         string = 'Earned {} gold from the cave! ({})'.format(session['cave_gold'], now)
         session['got_gold'].append(string)
-        session['mood'] = 'happy'
+        # session['mood'] = 'happy'
 
     if request.form['building'] == 'house':
         session['house_gold'] = random.randrange(2,6)
         session['gold_count'] += session['house_gold']
         string = 'Earned {} gold from the house! ({})'.format(session['house_gold'], now)
         session['got_gold'].append(string)
-        session['mood'] = 'happy'
+        # session['mood'] = 'happy'
 
     #random 0 or 1 will give 50/50 give/take value to casino gold
     give_take = random.randrange(0,2)
@@ -55,12 +63,12 @@ def gold_digger():
             session['gold_count'] += session['casino_gold']
             string = 'Winner!! You won {} gold at the casino! ({})'.format(session['casino_gold'], now)
             session['got_gold'].append(string)
-            session['mood'] = 'happy'
+            # session['mood'] = 'happy'
         else:
             session['gold_count'] -= session['casino_gold']
             string = 'Ahhhh, too bad... you lost {} gold from the casino! ({})'.format(session['casino_gold'], now)
             session['lost_gold'].append(string)
-            session['mood'] = 'sad'
+            # session['mood'] = 'sad'
 
     return redirect('/')
 
