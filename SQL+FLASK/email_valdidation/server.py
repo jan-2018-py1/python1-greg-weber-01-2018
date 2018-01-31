aeb890d3c5227b2c449b9d4307dca8350144e9f1
Greg Weber <gregoryweberguitars@gmail.com>
2018-01-30 18:23:11 -0800

    first commit - all running

---
+++ SQL+FLASK/email_valdidation/server.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, redirect, request, session, flash
+from mysqlconnection import MySQLConnector
+import re
+
+app = Flask(__name__)
+mysql = MySQLConnector(app, 'email_valid')
+
+# variable for email valid regular expression check
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+
+app.secret_key = 'one_life'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/process',methods=['POST'])
+def process():
+    # validate
+    error = False
+    error_message = 'Email is not valid!'
+    if EMAIL_REGEX.match(request.form['email']):
+        # write insert querry
+        query = "INSERT INTO users (email, created_at, updated_at) VALUES (:email, NOW(), NOW())"
+        # grab data from form
+        data = {
+            'email' : request.form['email']
+        }
+        #connect to mysql db
+        mysql.query_db(query, data)
+
+        # select all from db
+        emails = mysql.query_db('SELECT * FROM users')
+        return render_template('success.html', all_emails=emails)
+    else:
+        flash(error_message)
+    return redirect('/')
+    # if valid insert ito database
+
+
+app.run(debug=True)
