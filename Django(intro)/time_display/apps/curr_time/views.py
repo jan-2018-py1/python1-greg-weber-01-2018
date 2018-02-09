200ce24c26ce16c042bcb68d88636869145de7b0
Greg Weber <gregoryweberguitars@gmail.com>
2018-02-08 19:07:18 -0800

    first djaong project where static and templates are added - page displaying current time

---
+++ Django(intro)/time_display/apps/curr_time/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render, HttpResponse, redirect
+from time import gmtime, strftime, localtime
+
+def index(request):
+  context = {
+  "time": strftime("%b %d, %I:%M %p", localtime())
+  }
+  return render(request,'curr_time/index.html', context)
