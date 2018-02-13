0778d3e4d37dd863d915e54e1490042aa33fc959
Greg Weber <gregoryweberguitars@gmail.com>
2018-02-13 11:03:57 -0800

    ok

--- Django(intro)/django_apps/main/apps/random_word/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from . import views          
-urlpatterns = [
-    url(r'^random_word/$', views.random_word),
-    url(r'^random_word/generate_new$', views.generate_new),
-    url(r'^random_word/reset$', views.reset_counter)    
-  ]
--- Django(intro)/time_display/apps/curr_time/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render, HttpResponse, redirect
-from time import gmtime, strftime, localtime
-
-def index(request):
-  context = {
-  "time": strftime("%b %d, %Y %I:%M %p", localtime())
-  }
-  return render(request,'curr_time/index.html', context)
