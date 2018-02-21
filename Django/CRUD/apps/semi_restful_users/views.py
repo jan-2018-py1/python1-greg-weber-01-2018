70738aa870db19bf9f49180adc9e73fd5a098a09
Greg Weber <gregoryweberguitars@gmail.com>
2018-02-20 16:24:45 -0800

    added destroy route, added edit page/routing - all finished

--- Django/CRUD/apps/semi_restful_users/views.py
+++ Django/CRUD/apps/semi_restful_users/views.py
@@ -4,30 +4,48 @@ from .models import *
 # Create your views here.
 def index(req):
     users = User.objects.all()
+    print 'users:', users
     context = {
         'users': users
     }
-
     return render(req, 'semi_restful_users/index.html', context)
 
 
 def new(req):
-
+   
     return render(req, 'semi_restful_users/new.html')
 
 def edit(req, id):
-
-    return render(req, 'semi_restful_users/edit.html')
+    user = User.objects.get(id=id)
+    context = {
+        'this_user': user
+    }
+    return render(req, 'semi_restful_users/edit.html', context)
 
 def show(req, id):
-
-    return render(req, 'semi_restful_users/new.html')
+    user = User.objects.get(id=id)
+    #if post from edit --> get info from update post and update
+    if req.method == 'POST':
+        user.name = req.POST['name']  
+        user.email = req.POST['email']
+        user.save()
+    context = {
+        'this_user': user
+    }
+    return render(req, 'semi_restful_users/show.html', context)
 
 def create(req):
-
-    return redirect('/users/<id>')
+    User.objects.create(name=req.POST['name'], email=req.POST['email'])
+    new_user = User.objects.last() 
+    user_id = new_user.id
+    print "---"*30
+    print 'user id:', user_id
+    url = '/users/{}'.format(user_id)
+    return redirect(url)
 
 def destroy(req, id):
+    this_user = User.objects.get(id=id)
+    this_user.delete()
     return redirect('/users')
 
 def update(req, id):
