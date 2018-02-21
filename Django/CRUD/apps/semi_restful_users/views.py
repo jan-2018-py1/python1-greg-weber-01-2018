073525bae2d0ea75ea76a0c14185de0f8db9238e
Greg Weber <gregoryweberguitars@gmail.com>
2018-02-20 18:41:16 -0800

    fixed the post route from edit form to the update route by way of setting a session id when you land on the edit page

--- Django/CRUD/apps/semi_restful_users/views.py
+++ Django/CRUD/apps/semi_restful_users/views.py
@@ -10,12 +10,12 @@ def index(req):
     }
     return render(req, 'semi_restful_users/index.html', context)
 
-
 def new(req):
-   
     return render(req, 'semi_restful_users/new.html')
 
 def edit(req, id):
+    if 'user_id' not in req.session:
+        req.session['user_id'] = id
     user = User.objects.get(id=id)
     context = {
         'this_user': user
@@ -24,11 +24,6 @@ def edit(req, id):
 
 def show(req, id):
     user = User.objects.get(id=id)
-    #if post from edit --> get info from update post and update
-    if req.method == 'POST':
-        user.name = req.POST['name']  
-        user.email = req.POST['email']
-        user.save()
     context = {
         'this_user': user
     }
@@ -38,8 +33,6 @@ def create(req):
     User.objects.create(name=req.POST['name'], email=req.POST['email'])
     new_user = User.objects.last() 
     user_id = new_user.id
-    print "---"*30
-    print 'user id:', user_id
     url = '/users/{}'.format(user_id)
     return redirect(url)
 
@@ -48,8 +41,14 @@ def destroy(req, id):
     this_user.delete()
     return redirect('/users')
 
-def update(req, id):
-    return redirect(req, 'semi_restful_users/users/<id>')
+def update(req):
+    user = User.objects.get(id=req.session['user_id'])
+    user.name = req.POST['name']  
+    user.email = req.POST['email']
+    user.save()
+    user_id = req.session['user_id']
+    url = '/users/{}'.format(user_id)
+    return redirect(url)
 
 
 
